d51f0ca55a267cbc29866013c830eb3cbc789c84
focho27 <gerorod1999@gmail.com>
2021-02-23 10:24:04 -0300

    first commit: basis of project

---
+++ src/main/java/com/alkemy/springboot/challenge/data/app/SpringBootChallengeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.springboot.challenge.data.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChallengeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChallengeJavaApplication.class, args);
+	}
+
+}
